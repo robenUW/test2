96e0419d28e24443e6f555692a9cb7d388938aaf
robenUW <112424694+robenUW@users.noreply.github.com>
2023-11-28 11:00:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+let map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        },
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation.",
+  );
+}
+const request = {
+  query: "Crossfit",
+  fields: ["name", "geometry"],
+};
+
+service = new google.maps.places.PlacesService(map);
+service.findPlaceFromQuery(request, (results, status) => {
+  if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+    for (let i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+
+    map.setCenter(results[0].geometry.location);
+  }
+});
+
+function createMarker(place) {
+if (!place.geometry || !place.geometry.location) return;
+
+const marker = new google.maps.Marker({
+  map,
+  position: place.geometry.location,
+});
+
+google.maps.event.addListener(marker, "click", () => {
+  infowindow.setContent(place.name || "");
+  infowindow.open(map);
+});
+}
+
+window.initMap = initMap;
